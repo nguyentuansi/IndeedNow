f16063627d0d5f70cd4332a901d25c150ac8b645
Si Nguyen <singuyen.it@gmail.com>
2021-01-09 14:14:29 +0700

    Update

--- scripts/update.py
+++ scripts/update.py
@@ -3,6 +3,10 @@ import sys
 import http.client
 import json
 
+with open('log.txt', 'r') as read_obj:
+    # Read all lines in the file one by one
+    lines = read_obj.readlines()
+    read_obj.close()
 
 def setup_custom_logger(name):
     formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
@@ -17,44 +21,44 @@ def setup_custom_logger(name):
     logger.addHandler(screen_handler)
     return logger
 
+def check_if_string_in_file(string_to_search):
+    # Open the file in read only mode
+    if any(string_to_search in s for s in lines):
+        return False
+    else:
+        return True
 
 logger = setup_custom_logger('mysite')
-logger.info('File updated!')
+logger.info('Fetched Latest News!')
 
 conn = http.client.HTTPSConnection("newscatcher.p.rapidapi.com")
-
 headers = {
     'x-rapidapi-key': "17b22fda86msh2dc480f3f737cdap16a605jsn15b1d0d7f2f7",
     'x-rapidapi-host': "newscatcher.p.rapidapi.com"
 }
-
 conn.request(
     "GET", "/v1/latest_headlines?topic=tech&lang=en&media=True", headers=headers)
-
 res = conn.getresponse()
-
 data = json.loads(res.read().decode("utf-8"))
-
-
 original_stdout = sys.stdout  # Save a reference to the original standard output
 
-with open('log.txt', 'a') as f:
-    sys.stdout = f  # Change the standard output to the file we created.
-    print("******************************************")
-    for article in data['articles']:
-        print(article["title"])
-        print(article["summary"])
-        print(article["link"])
-        print("---------------------")
-    print("******************************************")
-    sys.stdout = original_stdout  # Reset the standard output to its original value
+# with open('log.txt', 'a') as f:
+#     sys.stdout = f  # Change the standard output to the file we created.
+#     for article in data['articles']:
+#         print(article["_id"])
+#     sys.stdout = original_stdout  # Reset the standard output to its original value
 
 with open('README.md', 'a') as f:
     sys.stdout = f  # Change the standard output to the file we created.
     for article in data['articles']:
-        print('![{}]({} "{}")'.format(article["title"],article["media"], article["title"]))
-        print('[{}]({})'.format(article["title"],article["link"]))
-        print('\n')
-        print('> {}'.format(article["summary"]))
-        print('\n')
+        if(check_if_string_in_file(article["_id"])):
+            with open('log.txt', 'a') as f:
+                sys.stdout = f  # Change the standard output to the file we created.
+                print(article["_id"])
+                sys.stdout = original_stdout  # Reset the standard output to its original value
+            print('![{}]({} "{}")'.format(article["title"],article["media"], article["title"]))
+            print('[{}]({})'.format(article["title"],article["link"]))
+            print('\n')
+            print('> {}'.format(article["summary"]))
+            print('\n')
     sys.stdout = original_stdout  # Reset the standard output to its original value
